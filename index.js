228c7f90b6eb959345e9062a343359f6e6235e42
oyzq_glb <ouyangzhiqiang98_glb@gmail.com>
2019-06-20 13:02:38 +0800

    画板完成

--- index.js
+++ index.js
@@ -1,46 +1,158 @@
 var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
-
+var lineWidth = 5;
+let canvasHistory = [];
+let step = -1;
+var c1;
 autoSetCanvasSize(yyy)
 
 listenToMouse(yyy)
 
 
 var eraserEnabled = false
-eraser.onclick = function () {
-    eraserEnabled = true
-    // actions.className = 'actions x'
-    eraser.classList.add('active')
-    brush.classList.remove('active')
 
-}
-brush.onclick = function () {
-    eraserEnabled = false
-    // actions.className = 'actions'
-    brush.classList.add('active')
-    eraser.classList.remove('active')
+one.onclick = function () {
+    lineWidth = 3;
+    for (var i = 0; i < one.parentNode.children.length; i++) {
+        one.parentNode.children[i].classList.remove('active')
+    }
+    one.classList.add('active')
 }
 
-red.onclick = function () { 
+two.onclick = function () {
+    lineWidth = 6;
+    for (var i = 0; i < two.parentNode.children.length; i++) {
+        two.parentNode.children[i].classList.remove('active')
+    }
+    two.classList.add('active')
+}
+
+three.onclick = function () {
+    lineWidth = 9;
+    for (var i = 0; i < three.parentNode.children.length; i++) {
+        three.parentNode.children[i].classList.remove('active')
+    }
+    three.classList.add('active')
+}
+
+four.onclick = function () {
+    lineWidth = 12;
+    for (var i = 0; i < four.parentNode.children.length; i++) {
+        four.parentNode.children[i].classList.remove('active')
+    }
+    four.classList.add('active')
+}
+
+five.onclick = function () {
+    lineWidth = 15;
+    for (var i = 0; i < five.parentNode.children.length; i++) {
+        five.parentNode.children[i].classList.remove('active')
+    }
+    five.classList.add('active')
+}
+
+six.onclick = function () {
+    lineWidth = 18;
+    for (var i = 0; i < six.parentNode.children.length; i++) {
+        six.parentNode.children[i].classList.remove('active')
+    }
+    six.classList.add('active')
+}
+
+seven.onclick = function () {
+    lineWidth = 21;
+    for (var i = 0; i < seven.parentNode.children.length; i++) {
+        seven.parentNode.children[i].classList.remove('active')
+    }
+    seven.classList.add('active')
+}
+
+clear.onclick = function () {
+    context.clearRect(0, 0, yyy.width, yyy.height)
+}
+
+save.onclick = function () {
+    var url = yyy.toDataURL('image/png')
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url
+    a.download = '我的画板'
+    a.target = '_blank'
+    a.click()
+}
+
+cancel.onclick = function () {
+
+    if (step >= 0) {
+        step--;
+        context.clearRect(0, 0, yyy.width, yyy.height)
+        context.putImageData(canvasHistory[step], 0, 0)
+        console.log(step, context)
+    } else {
+        console.log('不能再继续撤销了');
+    }
+}
+
+black.onclick = function () {
+    context.strokeStyle = 'black'
+    for (var i = 0; i < black.parentNode.children.length; i++) {
+        black.parentNode.children[i].classList.remove('active')
+    }
+    black.classList.add('active')
+}
+
+red.onclick = function () {
     context.strokeStyle = 'red'
+    for (var i = 0; i < black.parentNode.children.length; i++) {
+        black.parentNode.children[i].classList.remove('active')
+    }
     red.classList.add('active')
-    green.classList.remove('active')
-    blue.classList.remove('active')
- }
+}
 
- green.onclick = function () { 
-    context.strokeStyle= 'green'
+green.onclick = function () {
+    context.strokeStyle = 'green'
+    for (var i = 0; i < black.parentNode.children.length; i++) {
+        black.parentNode.children[i].classList.remove('active')
+    }
     green.classList.add('active')
-    red.classList.remove('active')
-    blue.classList.remove('active')
- }
+}
 
- blue.onclick = function () { 
+blue.onclick = function () {
     context.strokeStyle = 'blue'
+    for (var i = 0; i < black.parentNode.children.length; i++) {
+        black.parentNode.children[i].classList.remove('active')
+    }
     blue.classList.add('active')
-    red.classList.remove('active')
-    green.classList.remove('active')
- }
+}
+
+yellow.onclick = function () {
+    context.strokeStyle = 'yellow'
+    for (var i = 0; i < black.parentNode.children.length; i++) {
+        black.parentNode.children[i].classList.remove('active')
+    }
+    yellow.classList.add('active')
+}
+skyblue.onclick = function () {
+    context.strokeStyle = 'skyblue'
+    for (var i = 0; i < black.parentNode.children.length; i++) {
+        black.parentNode.children[i].classList.remove('active')
+    }
+    skyblue.classList.add('active')
+}
+pink.onclick = function () {
+    context.strokeStyle = 'pink'
+    for (var i = 0; i < black.parentNode.children.length; i++) {
+        black.parentNode.children[i].classList.remove('active')
+    }
+    pink.classList.add('active')
+}
+white.onclick = function () {
+    context.strokeStyle = 'white'
+    for (var i = 0; i < black.parentNode.children.length; i++) {
+        black.parentNode.children[i].classList.remove('active')
+    }
+    white.classList.add('active')
+}
 
 /******/
 
@@ -70,7 +182,7 @@ function drawCircle(x, y, radius) {
 function drawLine(x1, y1, x2, y2) {
     context.beginPath();
     context.moveTo(x1, y1) // 起点
-    context.lineWidth = 5
+    context.lineWidth = lineWidth
     context.lineTo(x2, y2) // 终点
     context.stroke()
     context.closePath()
@@ -122,6 +234,8 @@ function listenToMouse(canvas) {
         }
         canvas.onmouseup = function (aaa) {
             using = false
+            step++;
+            canvasHistory.push(context.getImageData(0, 0, yyy.width, yyy.height))
         }
 
     } else {
@@ -161,6 +275,8 @@ function listenToMouse(canvas) {
 
         canvas.ontouchend = function () {
             using = false
+            step++;
+            canvasHistory.push(context.getImageData(0, 0, yyy.width, yyy.height))
         }
     }
 }
